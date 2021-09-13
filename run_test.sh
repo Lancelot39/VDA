19787095ac02b15f09d4973c0686b1b7f79f4087
Lancelot39 <1046946079@QQ.com>
2021-09-13 16:41:10 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -2,3 +2,8 @@ python bert_robust.py --data_path Robust/yelp.txt --tgt_path saved/yelp_vda.pt
 python bert_robust.py --data_path Robust/yelp.txt --tgt_path saved/yelp_vda_freelb.pt
 python bert_robust.py --data_path Robust/yelp.txt --tgt_path saved/yelp_vda_smix.pt
 python bert_robust.py --data_path Robust/yelp.txt --tgt_path saved/yelp_vda_smart.pt
+
+python bert_pair_robust.py --data_path Robust/qnli.txt --tgt_path saved/qnli_vda.pt
+python bert_pair_robust.py --data_path Robust/qnli.txt --tgt_path saved/qnli_vda_freelb.pt
+python bert_pair_robust.py --data_path Robust/qnli.txt --tgt_path saved/qnli_vda_smart.pt
+python bert_pair_robust.py --data_path Robust/qnli.txt --tgt_path saved/qnli_vda_smix.pt
